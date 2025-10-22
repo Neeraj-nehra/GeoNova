c3a0c8f1ef82f1cb8b3d8064417556e6db1b5ee8
Neeraj Kumar <neerajnehra2510@gmail.com>
2025-10-22 10:47:48 +0000

    make this background image like this shown at landing page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,7 +5,7 @@ export default function LoginPage() {
   return (
     <main className="relative flex h-screen flex-col items-center justify-center p-4">
        <Image
-        src="https://picsum.photos/seed/login-bg/1920/1080"
+        src="https://picsum.photos/seed/green-mountains/1920/1080"
         alt="Mountain background"
         fill
         className="object-cover -z-10"
